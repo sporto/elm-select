46994a948a9ca5c75c40fe03a068054698ac3f10
Sebastian <Sebastian@Sebastian-3.local>
2016-12-11 20:11:54 +1100

    Add fuzzy matching

--- src/Select/Select.elm
+++ src/Select/Select.elm
@@ -1,20 +1,44 @@
 module Select.Select exposing (..)
 
+import Fuzzy
 import Html exposing (..)
 import Html.Attributes exposing (value)
 import Html.Events exposing (onInput)
-import Select.Models as Models
 import Select.Messages as Messages
+import Select.Models as Models
 import Select.Option
+import String
+import Tuple
 
 
 view : Models.Config msg item -> Models.Model item -> List item -> Html (Messages.Msg item)
-view config model options =
-    div []
-        [ div
-            [ onInput Messages.OnQueryChange
+view config model items =
+    let
+        relevantItems =
+            matchedItems config model items
+    in
+        div []
+            [ div
+                [ onInput Messages.OnQueryChange
+                ]
+                [ input [ value model.query ] []
+                ]
+            , div [] (List.map (Select.Option.view config) relevantItems)
             ]
-            [ input [ value model.query ] []
-            ]
-        , div [] (List.map (Select.Option.view config) options)
-        ]
+
+
+matchedItems : Models.Config msg item -> Models.Model item -> List item -> List item
+matchedItems config model items =
+    if model.query == "" then
+        []
+    else
+        let
+            scoreFor item =
+                Fuzzy.match [] [] (String.toLower model.query) (String.toLower (config.toLabel item))
+                    |> .score
+        in
+            items
+                |> List.map (\item -> ( scoreFor item, item ))
+                |> List.filter (\( score, item ) -> score < 100)
+                |> List.sortBy Tuple.first
+                |> List.map Tuple.second
